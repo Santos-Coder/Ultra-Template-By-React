49ae831380d197abb3f152f7dea268c2a3fcebb3
santos <santosrecovery@gmail.com>
2020-04-18 02:06:55 +0200

    Updates

---
+++ precache-manifest.923ca195649b5b2a7d2dbead11b46bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ebbf6fa5b90673dac8b0a71f8380f4",
+    "url": "/Ultra-Template-By-React/index.html"
+  },
+  {
+    "revision": "7ba42c59d6813265c19e",
+    "url": "/Ultra-Template-By-React/static/css/main.e392a923.chunk.css"
+  },
+  {
+    "revision": "5c66c1b86ef5c22cacb6",
+    "url": "/Ultra-Template-By-React/static/js/2.cf9391e0.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/Ultra-Template-By-React/static/js/2.cf9391e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ba42c59d6813265c19e",
+    "url": "/Ultra-Template-By-React/static/js/main.34ede79e.chunk.js"
+  },
+  {
+    "revision": "8171ee9967c7d0e9d87a",
+    "url": "/Ultra-Template-By-React/static/js/runtime-main.b6e67f67.js"
+  }
+]);
