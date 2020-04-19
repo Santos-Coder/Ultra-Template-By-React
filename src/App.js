a1aef068270a64e7d26639e676f8b76c4f9ae31e
santos <santosrecovery@gmail.com>
2020-04-19 18:40:02 +0200

    data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter , Route} from 'react-router-dom'
+import { HashRouter , Route} from 'react-router-dom'
 import './App.css';
 
 import Navbar from './Components/Navbar/Navbar'
@@ -11,11 +11,11 @@ import Index from './Components/Index/Index'
 class App extends Component {
     render () {
       return (
-        <BrowserRouter>
+        <HashRouter>
             <Navbar />
             <Route exact path='/' component={Index} />
             <Route path='/Contact' component={Contact} />
-        </BrowserRouter>
+        </HashRouter>
       );
     }
 }
